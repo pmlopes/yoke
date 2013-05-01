ef99021aecac24efb95d0553afe820dca9af3a0f
Paulo Lopes <pmlopes@gmail.com>
2013-05-01 13:37:32 +0200

    tests!!!

---
+++ src/test/java/com/jetdrone/vertx/yoke/test/middleware/VHost.java
@@ -0,0 +1,45 @@
+package com.jetdrone.vertx.yoke.test.middleware;
+
+import com.jetdrone.vertx.yoke.middleware.Vhost;
+import com.jetdrone.vertx.yoke.middleware.YokeHttpServerRequest;
+import com.jetdrone.vertx.yoke.test.Response;
+import com.jetdrone.vertx.yoke.test.YokeTester;
+import org.junit.Test;
+import org.vertx.java.core.CaseInsensitiveMultiMap;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.testtools.TestVerticle;
+
+import static org.vertx.testtools.VertxAssert.*;
+
+public class VHost extends TestVerticle {
+
+    @Test
+    public void testLimit() {
+        YokeTester yoke = new YokeTester(vertx);
+        yoke.use(new Vhost("*.com", new Handler<HttpServerRequest>() {
+            @Override
+            public void handle(HttpServerRequest request) {
+                request.response().end();
+                testComplete();
+            }
+        }));
+        yoke.use(new Handler<YokeHttpServerRequest>() {
+            @Override
+            public void handle(YokeHttpServerRequest request) {
+                request.response().end();
+                fail();
+            }
+        });
+
+        MultiMap headers = new CaseInsensitiveMultiMap();
+        headers.add("host", "www.mycorp.com");
+
+        yoke.request("GET", "/", headers, new Handler<Response>() {
+            @Override
+            public void handle(Response resp) {
+            }
+        });
+    }
+}
