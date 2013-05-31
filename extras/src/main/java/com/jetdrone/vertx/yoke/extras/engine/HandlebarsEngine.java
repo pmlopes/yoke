da17b6e37273db323181120a808a0ccffccbef80
Paulo Lopes <pmlopes@gmail.com>
2013-05-31 14:49:04 +0200

    #27 handlerbars template engine (extras)

---
+++ extras/src/main/java/com/jetdrone/vertx/yoke/extras/engine/HandlebarsEngine.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetdrone.vertx.yoke.extras.engine;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.jetdrone.vertx.yoke.Engine;
+import com.jetdrone.vertx.yoke.util.YokeAsyncResult;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Handler;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class HandlebarsEngine extends Engine<Template> {
+
+    private final Handlebars handlebars = new Handlebars();
+
+    @Override
+    public void render(final String filename, final Map<String, Object> context, final Handler<AsyncResult<String>> next) {
+        read(filename, new AsyncResultHandler<String>() {
+            @Override
+            public void handle(AsyncResult<String> asyncResult) {
+                if (asyncResult.failed()) {
+                    next.handle(new YokeAsyncResult<String>(asyncResult.cause()));
+                } else {
+                    try {
+                        Template template = compile(filename, asyncResult.result());
+                        next.handle(new YokeAsyncResult<>(template.apply(context)));
+                    } catch (IOException ex) {
+                        next.handle(new YokeAsyncResult<String>(ex));
+                    }
+                }
+            }
+        });
+    }
+
+    private Template compile(String filename, String templateText) throws IOException {
+
+        Template template = getTemplateFromCache(filename);
+
+        if (template == null) {
+            // real compile
+            template = handlebars.compileInline(templateText);
+            putTemplateToCache(filename, template);
+        }
+
+        return template;
+    }
+}
---
+++ extras/src/test/java/com/jetdrone/vertx/yoke/extras/test/engine/HandlebarsEngine.java
@@ -0,0 +1,100 @@
+package com.jetdrone.vertx.yoke.extras.test.engine;
+
+import com.jetdrone.vertx.yoke.Middleware;
+import com.jetdrone.vertx.yoke.middleware.YokeRequest;
+import com.jetdrone.vertx.yoke.test.Response;
+import com.jetdrone.vertx.yoke.test.YokeTester;
+import org.junit.Test;
+import org.vertx.java.core.Handler;
+import org.vertx.testtools.TestVerticle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.vertx.testtools.VertxAssert.*;
+
+public class HandlebarsEngine extends TestVerticle {
+
+    @Test
+    public void testEngine() {
+        try {
+            // create a temp template
+            File temp = File.createTempFile("template", ".html");
+            FileOutputStream out = new FileOutputStream(temp);
+            out.write("Hello {{name}}!".getBytes());
+            out.close();
+            final String location = temp.getAbsolutePath();
+
+            YokeTester yoke = new YokeTester(vertx);
+            yoke.engine("html", new com.jetdrone.vertx.yoke.extras.engine.HandlebarsEngine());
+            yoke.use(new Middleware() {
+                @Override
+                public void handle(YokeRequest request, Handler<Object> next) {
+                    request.put("name", "Paulo");
+                    request.response().render(location, next);
+                }
+            });
+
+            yoke.request("GET", "/", new Handler<Response>() {
+                @Override
+                public void handle(Response resp) {
+                    assertEquals(200, resp.getStatusCode());
+                    assertEquals("Hello Paulo!", resp.body.toString());
+                    testComplete();
+                }
+            });
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testEngine2() {
+        try {
+            // create a temp template
+            File temp = File.createTempFile("template", ".html");
+            FileOutputStream out = new FileOutputStream(temp);
+            out.write("<html><body><ul>{{#blogs}}<li>{{name}}</li>{{/blogs}}</ul></body></html>".getBytes());
+            out.close();
+            final String location = temp.getAbsolutePath();
+
+            YokeTester yoke = new YokeTester(vertx);
+            yoke.engine("html", new com.jetdrone.vertx.yoke.extras.engine.HandlebarsEngine());
+            yoke.use(new Middleware() {
+                @Override
+                public void handle(YokeRequest request, Handler<Object> next) {
+                    List<Map> blogs = new ArrayList<>();
+                    Map<String, String> blog1 = new HashMap<>();
+                    blog1.put("name", "Handlebars.java");
+                    blogs.add(blog1);
+
+                    Map<String, String> blog2 = new HashMap<>();
+                    blog2.put("name", "Handlebars.js");
+                    blogs.add(blog2);
+
+                    Map<String, String> blog3 = new HashMap<>();
+                    blog3.put("name", "Mustache");
+                    blogs.add(blog3);
+
+                    request.put("blogs", blogs);
+                    request.response().render(location, next);
+                }
+            });
+
+            yoke.request("GET", "/", new Handler<Response>() {
+                @Override
+                public void handle(Response resp) {
+                    assertEquals(200, resp.getStatusCode());
+                    assertEquals("<html><body><ul><li>Handlebars.java</li><li>Handlebars.js</li><li>Mustache</li></ul></body></html>", resp.body.toString());
+                    testComplete();
+                }
+            });
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
