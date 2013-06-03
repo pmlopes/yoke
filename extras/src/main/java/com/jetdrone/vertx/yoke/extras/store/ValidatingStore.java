95f49158cd15865c26e3277d0cc6041289444d81
Paulo Lopes <pmlopes@gmail.com>
2013-06-03 21:09:37 +0200

    started with validation store

--- extras/src/main/java/com/jetdrone/vertx/yoke/extras/store/ValidatingStore.java
+++ extras/src/main/java/com/jetdrone/vertx/yoke/extras/store/ValidatingStore.java
@@ -1,5 +1,7 @@
 package com.jetdrone.vertx.yoke.extras.store;
 
+import com.jetdrone.vertx.yoke.util.YokeAsyncResult;
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
@@ -13,45 +15,119 @@ public class ValidatingStore implements Store {
         this.baseStore = store;
     }
 
-    @Override
-    public void create(String entity, JsonObject object, AsyncResultHandler<String> response) {
-        // call before
-        baseStore.create(entity, object, response);
-        // call after
+    private static final YokeAsyncResult OK = new YokeAsyncResult(null, null);
+
+    public void beforeCreate(String entity, JsonObject object, AsyncResultHandler<String> response) {
+        response.handle(OK);
     }
 
     @Override
-    public void read(String entity, String id, AsyncResultHandler<JsonObject> response) {
-        // call before
-        baseStore.read(entity, id, response);
-        // call after
+    public void create(final String entity, final JsonObject object, final AsyncResultHandler<String> response) {
+        beforeCreate(entity, object, new AsyncResultHandler<String>() {
+            @Override
+            public void handle(AsyncResult<String> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.create(entity, object, response);
+            }
+        });
+    }
+
+    public void beforeRead(String entity, String id, AsyncResultHandler<JsonObject> response) {
+        response.handle(OK);
     }
 
     @Override
-    public void update(String entity, String id, JsonObject object, AsyncResultHandler<Number> response) {
-        // call before
-        baseStore.update(entity, id, object, response);
-        // call after
+    public void read(final String entity, final String id, final AsyncResultHandler<JsonObject> response) {
+        beforeRead(entity, id, new AsyncResultHandler<JsonObject>() {
+            @Override
+            public void handle(AsyncResult<JsonObject> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.read(entity, id, response);
+            }
+        });
+    }
+
+    public void beforeUpdate(String entity, String id, JsonObject object, AsyncResultHandler<Number> response) {
+        response.handle(OK);
     }
 
     @Override
-    public void delete(String entity, String id, AsyncResultHandler<Number> response) {
-        // call before
-        baseStore.delete(entity, id, response);
-        // call after
+    public void update(final String entity, final String id, final JsonObject object, final AsyncResultHandler<Number> response) {
+        beforeUpdate(entity, id, object, new AsyncResultHandler<Number>() {
+            @Override
+            public void handle(AsyncResult<Number> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.update(entity, id, object, response);
+            }
+        });
+    }
+
+    public void beforeDelete(String entity, String id, AsyncResultHandler<Number> response) {
+        response.handle(OK);
     }
 
     @Override
-    public void query(String entity, JsonObject query, Number start, Number end, JsonObject sort, AsyncResultHandler<JsonArray> response) {
-        // call before
-        baseStore.query(entity, query, start, end, sort, response);
-        // call after
+    public void delete(final String entity, final String id, final AsyncResultHandler<Number> response) {
+        beforeDelete(entity, id, new AsyncResultHandler<Number>() {
+            @Override
+            public void handle(AsyncResult<Number> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.delete(entity, id, response);
+            }
+        });
+    }
+
+    public void beforeQuery(String entity, JsonObject query, Number start, Number end, JsonObject sort, AsyncResultHandler<JsonArray> response) {
+        response.handle(OK);
     }
 
     @Override
-    public void count(String entity, JsonObject query, AsyncResultHandler<Number> response) {
-        // call before
-        baseStore.count(entity, query, response);
-        // call after
+    public void query(final String entity, final JsonObject query, final Number start, final Number end, final JsonObject sort, final AsyncResultHandler<JsonArray> response) {
+        beforeQuery(entity, query, start, end, sort, new AsyncResultHandler<JsonArray>() {
+            @Override
+            public void handle(AsyncResult<JsonArray> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.query(entity, query, start, end, sort, response);
+            }
+        });
+    }
+
+    public void beforeCount(String entity, JsonObject query, AsyncResultHandler<Number> response) {
+        response.handle(OK);
+    }
+
+    @Override
+    public void count(final String entity, final JsonObject query, final AsyncResultHandler<Number> response) {
+        beforeCount(entity, query, new AsyncResultHandler<Number>() {
+            @Override
+            public void handle(AsyncResult<Number> event) {
+                if (event.failed()) {
+                    response.handle(event);
+                    return;
+                }
+
+                baseStore.count(entity, query, response);
+            }
+        });
     }
 }
