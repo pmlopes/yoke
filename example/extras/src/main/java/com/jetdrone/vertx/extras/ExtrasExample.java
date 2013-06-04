3883a8de1525a07de2e76007b56c5958cee5d710
Paulo Lopes <pmlopes@gmail.com>
2013-06-04 13:35:05 +0200

    add example code for extras

--- example/extras/src/main/java/com/jetdrone/vertx/extras/ExtrasExample.java
+++ example/extras/src/main/java/com/jetdrone/vertx/extras/ExtrasExample.java
@@ -1,34 +1,40 @@
 package com.jetdrone.vertx.extras;
 
-import com.jetdrone.vertx.yoke.Middleware;
 import com.jetdrone.vertx.yoke.Yoke;
-import com.jetdrone.vertx.yoke.engine.StringPlaceholderEngine;
-import com.jetdrone.vertx.yoke.middleware.*;
-import com.jetdrone.vertx.yoke.util.Utils;
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.buffer.Buffer;
-import org.vertx.java.core.http.HttpClient;
-import org.vertx.java.core.http.HttpClientRequest;
-import org.vertx.java.core.http.HttpClientResponse;
-import org.vertx.java.core.json.DecodeException;
+import com.jetdrone.vertx.yoke.extras.engine.HandlebarsEngine;
+import com.jetdrone.vertx.yoke.extras.middleware.JsonRestRouter;
+import com.jetdrone.vertx.yoke.extras.store.MongoDbStore;
+import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
-
-import javax.crypto.Mac;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class ExtrasExample extends Verticle {
 
     @Override
     public void start() {
         final Yoke yoke = new Yoke(vertx);
-        yoke.engine("hbs", new StringPlaceholderEngine());
+        yoke.engine("hbs", new HandlebarsEngine());
 
+        JsonObject persistorCfg = new JsonObject();
+        persistorCfg.putString("host", "localhost");
+        persistorCfg.putNumber("port", 27017);
+        persistorCfg.putString("address", "mongo.persons");
+        persistorCfg.putString("db_name", "yoke");
+
+        final EventBus eb = vertx.eventBus();
+
+        // deploy mongo module
+        container.deployModule("io.vertx~mod-mongo-persistor~2.0.0-beta1", persistorCfg);
+
+        // db access
+        final MongoDbStore db = new MongoDbStore(eb, "mongo.persons");
+
+        JsonRestRouter router = new JsonRestRouter(db);
+        router.rest("/persons", "persons");
+
+        yoke.use(router);
         yoke.listen(8080);
+
         container.logger().info("Yoke server listening on port 8080");
     }
 }
