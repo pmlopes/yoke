b518ae858567002f10cfe654a6631c2c259c6d3f
Paulo Lopes <pmlopes@gmail.com>
2013-07-04 12:14:31 +0200

    added MVEL engine to the extras

---
+++ framework-extras/src/main/java/com/jetdrone/vertx/yoke/extras/engine/MVELEngine.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetdrone.vertx.yoke.extras.engine;
+
+import com.jetdrone.vertx.yoke.engine.AbstractEngine;
+import com.jetdrone.vertx.yoke.util.YokeAsyncResult;
+import org.mvel2.templates.CompiledTemplate;
+import org.mvel2.templates.TemplateCompiler;
+import org.mvel2.templates.TemplateRuntime;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MVELEngine extends AbstractEngine<CompiledTemplate> {
+
+    @Override
+    public void render(final String filename, final Map<String, Object> context, final Handler<AsyncResult<Buffer>> next) {
+        read(filename, new AsyncResultHandler<String>() {
+            @Override
+            public void handle(AsyncResult<String> asyncResult) {
+                if (asyncResult.failed()) {
+                    next.handle(new YokeAsyncResult<Buffer>(asyncResult.cause()));
+                } else {
+                    try {
+                        CompiledTemplate template = compile(filename, asyncResult.result());
+                        next.handle(new YokeAsyncResult<>(new Buffer((String) TemplateRuntime.execute(template, context))));
+                    } catch (IOException ex) {
+                        next.handle(new YokeAsyncResult<Buffer>(ex));
+                    }
+                }
+            }
+        });
+    }
+
+    private CompiledTemplate compile(String filename, String templateText) throws IOException {
+
+        CompiledTemplate template = getTemplateFromCache(filename);
+
+        if (template == null) {
+            // real compile
+            template = TemplateCompiler.compileTemplate(templateText);
+            putTemplateToCache(filename, template);
+        }
+
+        return template;
+    }
+}
---
+++ framework-extras/src/test/java/com/jetdrone/vertx/yoke/extras/test/engine/MVELEngine.java
@@ -0,0 +1,98 @@
+package com.jetdrone.vertx.yoke.extras.test.engine;
+
+import com.jetdrone.vertx.yoke.Middleware;
+import com.jetdrone.vertx.yoke.middleware.YokeRequest;
+import com.jetdrone.vertx.yoke.test.Response;
+import com.jetdrone.vertx.yoke.test.YokeTester;
+import org.junit.Test;
+import org.vertx.java.core.Handler;
+import org.vertx.testtools.TestVerticle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.vertx.testtools.VertxAssert.*;
+
+public class MVELEngine extends TestVerticle {
+
+    @Test
+    public void testBasicObjectAccess() {
+        try {
+            // create a temp template
+            File temp = File.createTempFile("template", ".html");
+            FileOutputStream out = new FileOutputStream(temp);
+            out.write("<h1>@{name}</h1>".getBytes());
+            out.close();
+            final String location = temp.getAbsolutePath();
+
+            YokeTester yoke = new YokeTester(vertx);
+            yoke.engine("html", new com.jetdrone.vertx.yoke.extras.engine.MVELEngine());
+            yoke.use(new Middleware() {
+                @Override
+                public void handle(YokeRequest request, Handler<Object> next) {
+                    request.put("name", "Paulo");
+                    request.response().render(location, next);
+                }
+            });
+
+            yoke.request("GET", "/", new Handler<Response>() {
+                @Override
+                public void handle(Response resp) {
+                    assertEquals(200, resp.getStatusCode());
+                    assertEquals("<h1>Paulo</h1>", resp.body.toString());
+                    testComplete();
+                }
+            });
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSimpleIteration() {
+        try {
+            // create a temp template
+            File temp = File.createTempFile("template", ".html");
+            FileOutputStream out = new FileOutputStream(temp);
+            out.write(("<p>@foreach{index : alphabetical}<a href=\"@{index.uri}\">@{index.description}</a>@end{}</p>").getBytes());
+            out.close();
+            final String location = temp.getAbsolutePath();
+
+            YokeTester yoke = new YokeTester(vertx);
+            yoke.engine("html", new com.jetdrone.vertx.yoke.extras.engine.MVELEngine());
+            yoke.use(new Middleware() {
+                @Override
+                public void handle(YokeRequest request, Handler<Object> next) {
+                    List<Map<String, String>> list = new ArrayList<>();
+                    Map<String, String> item = new HashMap<>();
+                    item.put("uri", "a");
+                    item.put("description", "b");
+                    list.add(item);
+
+                    item = new HashMap<>();
+                    item.put("uri", "c");
+                    item.put("description", "d");
+                    list.add(item);
+
+                    request.put("alphabetical", list);
+                    request.response().render(location, next);
+                }
+            });
+
+            yoke.request("GET", "/", new Handler<Response>() {
+                @Override
+                public void handle(Response resp) {
+                    assertEquals(200, resp.getStatusCode());
+                    assertEquals("<p><a href=\"a\">b</a><a href=\"c\">d</a></p>", resp.body.toString());
+                    testComplete();
+                }
+            });
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
