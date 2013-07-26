f3294a26dd00c30964bfb484f38d5c1a259e0da0
Paulo Lopes <pmlopes@gmail.com>
2013-07-26 10:10:14 +0200

    #50 make API more Groovy-ish by allowing Maps and Lists to be automatically serialized to JSON

--- framework/src/main/groovy/com/jetdrone/vertx/yoke/middleware/GYokeResponse.java
+++ framework/src/main/groovy/com/jetdrone/vertx/yoke/middleware/GYokeResponse.java
@@ -17,14 +17,21 @@ package com.jetdrone.vertx.yoke.middleware;
 
 import com.jetdrone.vertx.yoke.Engine;
 import groovy.lang.Closure;
+import org.vertx.groovy.core.impl.DefaultMultiMap;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.MultiMap;
+import org.vertx.groovy.core.MultiMap;
 import org.vertx.java.core.http.HttpServerResponse;
 import org.vertx.groovy.core.buffer.Buffer;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
 
+import java.util.List;
 import java.util.Map;
 
 public class GYokeResponse extends YokeResponse /*implements org.vertx.groovy.core.http.HttpServerResponse*/ {
+
+    private MultiMap headers;
+    private MultiMap trailers;
 
     public GYokeResponse(HttpServerResponse response, Map<String, Object> context, Map<String, Engine> engines) {
         super(response, context, engines);
@@ -79,14 +86,74 @@ public class GYokeResponse extends YokeResponse /*implements org.vertx.groovy.co
     }
 
     public MultiMap getHeaders() {
-        return headers();
+        if (headers == null) {
+            headers = new DefaultMultiMap(headers());
+        }
+        return headers;
     }
 
     public MultiMap getTrailers() {
-        return trailers();
+        if (trailers == null) {
+            trailers = new DefaultMultiMap(trailers());
+        }
+        return trailers;
     }
 
     public boolean isWriteQueueFull() {
         return writeQueueFull();
+    }
+
+    public void end(Map<String, Object> json) {
+        setContentType("application/json", "UTF-8");
+        end(new JsonObject(json).encode());
+    }
+
+    public void end(List<Object> json) {
+        setContentType("application/json", "UTF-8");
+        end(new JsonArray(json).encode());
+    }
+
+    public void jsonp(Map<String, Object> json) {
+        jsonp("callback", json);
+    }
+
+    public void jsonp(List<Object> json) {
+        jsonp("callback", json);
+    }
+
+    public void jsonp(String callback, Map<String, Object> json) {
+
+        if (callback == null) {
+            // treat as normal json response
+            end(json);
+            return;
+        }
+
+        String body = null;
+
+        if (json != null) {
+            JsonObject jsonObject = new JsonObject(json);
+            body = jsonObject.encode();
+        }
+
+        jsonp(callback, body);
+    }
+
+    public void jsonp(String callback, List<Object> json) {
+
+        if (callback == null) {
+            // treat as normal json response
+            end(json);
+            return;
+        }
+
+        String body = null;
+
+        if (json != null) {
+            JsonArray jsonArray = new JsonArray(json);
+            body = jsonArray.encode();
+        }
+
+        jsonp(callback, body);
     }
 }
