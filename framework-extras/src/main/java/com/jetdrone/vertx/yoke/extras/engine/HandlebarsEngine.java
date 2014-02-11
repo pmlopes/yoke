4d3468a39da9d6ae6dc1051ad0039df07fcfbd14
silviucm <silviucapota@hotmail.com>
2014-02-11 07:26:36 -0500

    Update HandlebarsEngine.java

--- framework-extras/src/main/java/com/jetdrone/vertx/yoke/extras/engine/HandlebarsEngine.java
+++ framework-extras/src/main/java/com/jetdrone/vertx/yoke/extras/engine/HandlebarsEngine.java
@@ -50,6 +50,13 @@ public class HandlebarsEngine extends AbstractEngine<Template> {
             }
         });
     }
+    
+    @Override
+    public void render(final String filename, final String layoutFilename, final Map<String, Object> context, final Handler<AsyncResult<Buffer>> next) {
+       
+    	// todo: implement proper layout support like in Groovy Template Engine   
+    	
+    }        
 
     private Template compile(String filename, String templateText) throws IOException {
 
