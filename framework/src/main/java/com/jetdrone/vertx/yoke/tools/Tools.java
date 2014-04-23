ea221dff91c18ebd422f96840d9bf4d035c5dba3
Paulo Lopes <pmlopes@gmail.com>
2014-04-23 15:03:56 +0200

    nex iteration

--- framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
@@ -10,7 +10,7 @@ public class Tools {
     private static String module;
     private static String version;
 
-    private static final String VERSION = "1.0.8";
+    private static final String VERSION = "1.0.9-SNAPSHOT";
 
     static void write(String path, String value) {
         try {
