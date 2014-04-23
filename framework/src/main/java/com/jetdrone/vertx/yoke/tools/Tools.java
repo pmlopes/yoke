81a149c825f029f7d9d67f2888caaad3f8b01759
Paulo Lopes <pmlopes@gmail.com>
2014-04-23 14:57:36 +0200

    prepare release 1.0.8

--- framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
@@ -10,6 +10,8 @@ public class Tools {
     private static String module;
     private static String version;
 
+    private static final String VERSION = "1.0.8";
+
     static void write(String path, String value) {
         try {
             FileOutputStream out = new FileOutputStream(path);
@@ -209,7 +211,7 @@ public class Tools {
         write(module + "/src/main/resources/mod.json",
                 "{\n" +
                 "  \"main\": \"" + owner + "." + module + ".App\",\n" +
-                "  \"includes\": \"com.jetdrone~yoke~1.0.8-SNAPSHOT\"\n" +
+                "  \"includes\": \"com.jetdrone~yoke~" + VERSION + "\"\n" +
                 "}\n");
 
         mkdir(module + "/src/test/java");
@@ -236,7 +238,7 @@ public class Tools {
         write(module + "/src/main/resources/mod.json",
                 "{\n" +
                         "  \"main\": \"App.groovy\",\n" +
-                        "  \"includes\": \"com.jetdrone~yoke~1.0.8-SNAPSHOT\"\n" +
+                        "  \"includes\": \"com.jetdrone~yoke~" + VERSION + "\"\n" +
                         "}\n");
     }
 
@@ -254,7 +256,7 @@ public class Tools {
         write(module + "/src/main/resources/mod.json",
                 "{\n" +
                         "  \"main\": \"App.js\",\n" +
-                        "  \"includes\": \"com.jetdrone~yoke~1.0.8-SNAPSHOT\"\n" +
+                        "  \"includes\": \"com.jetdrone~yoke~" + VERSION + "\"\n" +
                         "}\n");
     }
 
