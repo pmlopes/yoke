b4391c8bec418734af377cd111dbd2a9b0a556c5
Paulo Lopes <pmlopes@gmail.com>
2014-04-30 10:25:50 +0200

    release 1.0.10

--- framework/src/test/java/com/jetdrone/vertx/yoke/test/JWTTest.java
+++ framework/src/test/java/com/jetdrone/vertx/yoke/test/JWTTest.java
@@ -4,10 +4,13 @@ import com.jetdrone.vertx.yoke.util.JWT;
 import org.junit.Test;
 import org.vertx.testtools.TestVerticle;
 
+import static org.vertx.testtools.VertxAssert.testComplete;
+
 public class JWTTest extends TestVerticle {
 
     @Test
     public void testJWT() {
         JWT jwt = new JWT("secret");
+        testComplete();
     }
 }
