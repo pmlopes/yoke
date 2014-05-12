2888badac3f21c9454c6a4d50eeac60c65b75946
Paulo Lopes <pmlopes@gmail.com>
2014-05-12 12:49:54 +0200

    validate registered claims

--- framework/src/main/java/com/jetdrone/vertx/yoke/middleware/JWT.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/middleware/JWT.java
@@ -4,6 +4,7 @@ import com.jetdrone.vertx.yoke.Middleware;
 import com.jetdrone.vertx.yoke.core.YokeException;
 import org.jetbrains.annotations.NotNull;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonObject;
 
 import java.util.regex.Pattern;
 
@@ -62,6 +63,36 @@ public class JWT extends Middleware {
         }
 
         try {
+            final JsonObject jwtToken = jwt.decode(token);
+
+            final long now = System.currentTimeMillis();
+
+            if (jwtToken.containsField("iat")) {
+                Long iat = jwtToken.getLong("iat");
+                // issue at must be in the past
+                if (iat >= now) {
+                    next.handle(new YokeException(401, "Invalid Token!"));
+                    return;
+                }
+            }
+
+            if (jwtToken.containsField("nbf")) {
+                Long nbf = jwtToken.getLong("nbf");
+                // not before must be after now
+                if (nbf >= now) {
+                    next.handle(new YokeException(401, "Invalid Token!"));
+                    return;
+                }
+            }
+
+            if (jwtToken.containsField("exp")) {
+                Long exp = jwtToken.getLong("exp");
+                // expires must be after now
+                if (exp >= now) {
+                    next.handle(new YokeException(401, "Invalid Token!"));
+                    return;
+                }
+            }
             request.put("jwt", jwt.decode(token));
             next.handle(null);
         } catch (RuntimeException e) {
