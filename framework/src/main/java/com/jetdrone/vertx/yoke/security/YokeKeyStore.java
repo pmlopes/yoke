2315af2ccb6449ce945c7b791d75378c14dca474
Paulo Lopes <pmlopes@gmail.com>
2014-05-12 10:25:42 +0200

    unified keystore access

--- framework/src/main/java/com/jetdrone/vertx/yoke/security/YokeKeyStore.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/security/YokeKeyStore.java
@@ -11,19 +11,12 @@ import java.security.cert.*;
 
 public final class YokeKeyStore {
 
-    private final KeyStore ks = getKeyStore();
+    private final KeyStore ks;
 
-    private static KeyStore getKeyStore() throws KeyStoreException {
-        try {
-            // try to load the Java Crypto Extensions
-            return KeyStore.getInstance("jceks");
-        } catch (KeyStoreException e0) {
-            // fallback to default
-            return KeyStore.getInstance(KeyStore.getDefaultType());
-        }
-    }
+    public YokeKeyStore(@NotNull String storeType, @NotNull String fileName, @NotNull String keyStorePassword) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
 
-    public YokeKeyStore(@NotNull String fileName, @NotNull String keyStorePassword) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        ks = KeyStore.getInstance(storeType);
+
         try (InputStream in = getClass().getResourceAsStream(fileName)) {
             if (in == null) {
                 throw new FileNotFoundException(fileName);
@@ -31,6 +24,9 @@ public final class YokeKeyStore {
 
             ks.load(in, keyStorePassword.toCharArray());
         }
+    }
+    public YokeKeyStore(@NotNull String fileName, @NotNull String keyStorePassword) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        this(KeyStore.getDefaultType(), fileName, keyStorePassword);
     }
 
     public SecretKey getSecretKey(@NotNull String alias, @NotNull String keyPassword) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
@@ -40,29 +36,29 @@ public final class YokeKeyStore {
     public Cipher getCipher(@NotNull String alias, @NotNull String keyPassword, int mode) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchPaddingException, InvalidKeyException {
         final SecretKey secretKey = getSecretKey(alias, keyPassword);
 
-        Cipher c = Cipher.getInstance(secretKey.getAlgorithm());
-        c.init(mode, secretKey);
+        Cipher cipher = Cipher.getInstance(secretKey.getAlgorithm());
+        cipher.init(mode, secretKey);
 
-        return c;
+        return cipher;
     }
 
     public Mac getMac(@NotNull String alias, @NotNull String keyPassword) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchPaddingException, InvalidKeyException {
         final SecretKey secretKey = getSecretKey(alias, keyPassword);
 
-        Mac hmac = Mac.getInstance(secretKey.getAlgorithm());
-        hmac.init(secretKey);
+        Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+        mac.init(secretKey);
 
-        return hmac;
+        return mac;
     }
 
     public Signature getSignature(@NotNull String alias, @NotNull String keyPassword) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, InvalidKeyException {
         final PrivateKey privateKey = (PrivateKey) getKey(alias, keyPassword);
         final X509Certificate certificate = (X509Certificate) ks.getCertificate(alias);
 
-        Signature instance = Signature.getInstance(certificate.getSigAlgName());
-        instance.initSign(privateKey);
+        Signature signature = Signature.getInstance(certificate.getSigAlgName());
+        signature.initSign(privateKey);
 
-        return instance;
+        return signature;
     }
 
     public Key getKey(@NotNull String alias, @NotNull String keyPassword) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
