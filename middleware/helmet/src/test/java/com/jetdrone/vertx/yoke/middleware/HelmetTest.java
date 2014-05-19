258dca2c16011eafc0bc303c3b77cd1b10e58da8
Paulo Lopes <pmlopes@gmail.com>
2014-05-19 10:17:20 +0200

    add security middleware

---
+++ middleware/helmet/src/test/java/com/jetdrone/vertx/yoke/middleware/HelmetTest.java
@@ -0,0 +1,14 @@
+package com.jetdrone.vertx.yoke.middleware;
+
+import org.junit.Test;
+import org.vertx.testtools.TestVerticle;
+
+import static org.vertx.testtools.VertxAssert.*;
+
+public class HelmetTest extends TestVerticle {
+
+    @Test
+    public void test() {
+        testComplete();
+    }
+}
