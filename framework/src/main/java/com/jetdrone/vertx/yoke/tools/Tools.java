dcff61f2be878b6b03fff980a671c864c306ea2b
Paulo Lopes <pmlopes@gmail.com>
2014-05-22 11:29:38 +0200

    prepare release

--- framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/tools/Tools.java
@@ -10,7 +10,7 @@ public class Tools {
     private static String module;
     private static String version;
 
-    private static final String VERSION = "1.0.16";
+    private static final String VERSION = "1.0.17";
 
     static void write(String path, String value) {
         try {
