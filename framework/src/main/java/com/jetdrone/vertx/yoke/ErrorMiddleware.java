feea334660fed60b89335d3047648c3f92bc0b04
Paulo Lopes <pmlopes@gmail.com>
2014-10-07 10:17:17 +0200

    clean up

--- framework/src/main/java/com/jetdrone/vertx/yoke/ErrorMiddleware.java
+++ framework/src/main/java/com/jetdrone/vertx/yoke/ErrorMiddleware.java
@@ -3,5 +3,5 @@ package com.jetdrone.vertx.yoke;
 /**
  * This is just a markup in order to identify if a Middleware is to handle errors or not.
  */
-public interface ErrorMiddleware extends Middleware {
+public interface ErrorMiddleware {
 }
