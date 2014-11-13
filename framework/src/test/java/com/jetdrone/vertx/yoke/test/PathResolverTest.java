2507ce4494ca2ba2b2f5fb8c8b68ab3415f1d174
Paulo Lopes <pmlopes@gmail.com>
2014-11-13 15:05:01 +0100

    #119 all included now

--- framework/src/test/java/com/jetdrone/vertx/yoke/test/PathResolverTest.java
+++ framework/src/test/java/com/jetdrone/vertx/yoke/test/PathResolverTest.java
@@ -7,6 +7,7 @@ import org.vertx.java.core.impl.VertxInternal;
 import org.vertx.testtools.TestVerticle;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.vertx.testtools.VertxAssert.*;
 
@@ -14,7 +15,17 @@ public class PathResolverTest extends TestVerticle {
 
     @Test
     @Ignore
-    public void testEscape() {
+    public void testEscape() throws IOException {
+
+        String base = PathAdjuster.adjust((VertxInternal) vertx, ".");
+
+        File file1 = new File(base, "static/dir1/file.1");
+        file1.getParentFile().mkdirs();
+        file1.createNewFile();
+
+        File file2 = new File(base, "static/dir1/new file.1");
+        file2.getParentFile().mkdirs();
+        file2.createNewFile();
 
         System.out.println(PathAdjuster.adjust((VertxInternal) vertx, "static/dir1/file.1"));
         // /home/paulo/Projects/yoke/framework/build/resources/test/static/dir1/file.1
